1af8238f55cc4ef655b35d940d64718870ee2365
milyasarmans <milyasarmans13@gmail.com>
2023-09-12 13:32:18 +0700

    get user's payment transaction endpoint

--- handler/transaction.go
+++ handler/transaction.go
@@ -63,8 +63,39 @@ func (h *transactionHandler) GetUserTransactions(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 }
 
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatError(err)
+
+		errorMessage := gin.H{"error": errors}
+
+		response := helper.APIResponse("Failed to create transaction", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	newTransaction, err := h.service.CreateTransaction(input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+
+		return
+	}
+
+	response := helper.APIResponse("Success to create transaction", http.StatusOK, "success", transaction.FormatTransaction(newTransaction))
+	c.JSON(http.StatusOK, response)
+}
+
 // GetUserTransactions
 // handler
 // ambil nilai user dari jwt
-// service
+// service, ambil dari midtrans
 // repository ambil data transaction
---
+++ payment/service.go
@@ -0,0 +1,47 @@
+package payment
+
+import (
+	"raisesync/user"
+	"strconv"
+
+	"github.com/veritrans/go-midtrans"
+)
+
+type service struct{}
+
+type Service interface {
+	GetPaymentURL(transaction Transaction, user user.User) (string, error)
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = "SB-Mid-server-_LMc6fl4Tcjjm7TdiltBWGKF"
+	midclient.ClientKey = "SB-Mid-client-ew7pY3sGWyc04ZNU"
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		CustomerDetail: &midtrans.CustDetail{
+			Email: user.Email,
+			FName: user.Name,
+		},
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+	}
+
+	snapTokenResp, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		return "", err
+	}
+
+	return snapTokenResp.RedirectURL, nil
+}
